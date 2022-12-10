f006e1ccdce1fd193a45b7db018d4b0062f17df6
Ubuntu <ubuntu@ip-172-31-36-80.ap-northeast-2.compute.internal>
2022-12-10 06:57:48 +0000

    Add pr creation automation

---
+++ scripts/create-pr.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+GITHUB_BOT_EMAIL = "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
+
+branch_name = str(hash(datetime.now().timestamp()))
+os.system(f'git checkout -b {branch_name}')
+os.system(f'git add ./default-policies/*')
+os.system(f'GIT_COMMITTER_NAME="GITHUB_BOT" git commit --author={GITHUB_BOT_EMAIL}')
+os.system(f'git push origin {branch_name}')
+
+PR_TITLE = "[Automation testing]"
+PR_BODY = "Test"
+
+# Should set 'GITHUB_TOKEN' as environment variable.
+os.system(' '.join([
+  f'gh pr create',
+  f'--title "{PR_TITLE}"',
+  f'--body "{PR_BODY}"',
+  f'--repo "lablup/backend.ai-jail"',
+  f'--base "main"',
+  f'--draft',
+  f'--label "infrastructure"'
+]))
