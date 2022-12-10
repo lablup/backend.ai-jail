1ae17f26325492426044314be7a13ae7568458e7
Ubuntu <ubuntu@ip-172-31-36-80.ap-northeast-2.compute.internal>
2022-12-10 08:15:43 +0000

    Update update-profile.yml

--- scripts/create-pr.py
+++ scripts/create-pr.py
@@ -5,11 +5,11 @@ GITHUB_BOT_EMAIL = "github-actions[bot] <41898282+github-actions[bot]@users.nore
 branch_name = str(hash(datetime.now().timestamp()))
 os.system(f'git checkout -b {branch_name}')
 os.system(f'git add ./default-policies/*')
-os.system(f'GIT_COMMITTER_NAME="GITHUB_BOT" git commit --author={GITHUB_BOT_EMAIL}')
+os.system(f'GIT_COMMITTER_NAME="PROFILE_UPDATE_BOT" git commit --author={GITHUB_BOT_EMAIL}')
 os.system(f'git push origin {branch_name}')
 
-PR_TITLE = "[Automation testing]"
-PR_BODY = "Test"
+PR_TITLE = "chore: bump default policy update"
+PR_BODY = "Bumps [seccomp default policy](https://raw.githubusercontent.com/moby/moby/master/profiles/seccomp/default.json) update"
 
 os.system(' '.join([
   f'GITHUB_TOKEN={os.environ["GH_TOKEN"]}',
@@ -18,6 +18,7 @@ os.system(' '.join([
   f'--body "{PR_BODY}"',
   f'--repo "lablup/backend.ai-jail"',
   f'--base "main"',
-  f'--draft',
-  f'--label "infrastructure"'
+  f'--label "infrastructure"',
+  # TODO: Remove below flag.
+  f'--draft'
 ]))
