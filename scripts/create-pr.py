c8d2396e0cd9b109bccf4a200be181033915ec6b
Ubuntu <ubuntu@ip-172-31-5-82.ap-northeast-2.compute.internal>
2023-01-07 06:32:59 +0000

    Fix CI

--- scripts/create-pr.py
+++ scripts/create-pr.py
@@ -1,19 +1,21 @@
 import os
 import subprocess
 from datetime import datetime
+from pathlib import Path
 
 GITHUB_BOT_EMAIL = (
     "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
 )
 
 branch_name = str(hash(datetime.now().timestamp()))
-os.system(f"git checkout -b {branch_name}")
-subprocess.run(f"git add ./default-policies/*", check=True)
+subprocess.run(f"git checkout -b {branch_name}", check=True, cwd=Path.cwd())
+subprocess.run(f"git add ./default-policies/*", check=True, cwd=Path.cwd())
 subprocess.run(
     f'GIT_COMMITTER_NAME="PROFILE_UPDATE_BOT" git commit --author="{GITHUB_BOT_EMAIL}" -m "Update Seccomp Default Profiles"',
     check=True,
+    cwd=Path.cwd(),
 )
-subprocess.run(f"git push origin {branch_name}")
+subprocess.run(f"git push origin {branch_name}", check=True, cwd=Path.cwd())
 
 PR_TITLE = "chore: bump default policy update"
 PR_BODY = "Bumps [seccomp default policy](https://raw.githubusercontent.com/moby/moby/master/profiles/seccomp/default.json) update"
@@ -33,4 +35,5 @@ subprocess.run(
         ]
     ),
     check=True,
+    cwd=Path.cwd(),
 )
