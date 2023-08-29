0d8e564635d8dd72797ef77feb8a0e2bf8435bb0
Kyujin Cho <kyujin.cho@lablup.com>
2023-08-29 03:03:47 +0000

    fix script not working

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
-mkdir -p dist
+if [ $(uname) = "Darwin" ]; then
+    readlink="greadlink"
+    dirname="gdirname"
+else
+    readlink="readlink"
+    dirname="dirname"
+fi
+
+PROJECT_ROOT=$($dirname $($dirname "$($readlink -f "$0")"))
 PLATFORM=$1
+
+mkdir -p dist
+
 case "$PLATFORM" in
 "alpine") DOCKER_TAG="alpine";;
 "ubuntu18.04") DOCKER_TAG="buster";;
 "ubuntu20.04") DOCKER_TAG="bullseye";;
 "ubuntu22.04") DOCKER_TAG="bookworm";;
+"buster") DOCKER_TAG="buster";;
+"bullseye") DOCKER_TAG="bullseye";;
+"bookworm") DOCKER_TAG="bookworm";;
 *) echo "Unsupported Platform $1"; exit 1;;
 esac
 
-docker run --rm -u $(id -u):$(id -g) -e PLATFORM=$PLATFORM -v $PROJECT_ROOT:/io rust:$DOCKER_TAG /io/scripts/build.sh
+docker run --rm -e FILEUSER="$(id -u):$(id -g)" -e PLATFORM=$PLATFORM -v $PROJECT_ROOT:/io rust:$DOCKER_TAG /io/scripts/build.sh
 ls dist/
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-export CARGO_HOME=$(mktemp -d)
-export RUSTUP_HOME=$(mktemp -d)
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-PATH="$HOME/.cargo/bin:$PATH"
-
 ARCHITECTURE=$(uname -m)
+if [ $ARCHITECTURE = "arm64" ]; then
+    ARCHITECTURE="aarch64"
+fi
+apt update && apt install -y libseccomp-dev
+
+cd /io
 cargo build --release
 cp /io/target/release/backendai-jail /io/dist/backendai-jail.$PLATFORM.$ARCHITECTURE.bin
+chown $FILEUSER /io/dist/backendai-jail.$PLATFORM.$ARCHITECTURE.bin
+rm -r /io/target/release
