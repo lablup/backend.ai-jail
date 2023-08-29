ce9c710f4d13e0233d2645713aa1795921b29ed0
Kyujin Cho <kyujin.cho@lablup.com>
2023-08-29 05:59:37 +0000

    fix script failing on alpine

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
+#! /bin/sh
 
 ARCHITECTURE=$(uname -m)
 if [ $ARCHITECTURE = "arm64" ]; then
     ARCHITECTURE="aarch64"
 fi
-apt update && apt install -y libseccomp-dev
 
 cd /io
 cargo build --release
